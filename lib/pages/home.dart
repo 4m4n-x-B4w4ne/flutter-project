971a76cf80c167d0345c93ad0beb9fdbc976f22b
4m4n-x-B4w4ne <125849251+4m4n-x-B4w4ne@users.noreply.github.com>
2023-03-10 10:17:00 +0530

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_application_group/pages/login.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(child: Text("WELCOME TO HOME PAGE")),
+    final Iage ="https://teqip.in/wp-content/uploads/2022/11/Photo-by-Aman-Gupta-on-January-22-2022_61ffafd14f1e7-1.png";
+    return Scaffold(
+      
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        titleTextStyle: TextStyle(),
+        title: Text("Dashboard",style: TextStyle(
+          
+        ),),
+      ),
+      drawer: Drawer(
+        
+        backgroundColor: Colors.black,
+        
+          child: ListView(
+            children: 
+            [
+            DrawerHeader(child: 
+            
+              UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.deepOrange),
+                
+                margin: EdgeInsets.zero,
+                accountName: Text(""),accountEmail:Text("") ,
+                currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage(Iage),)
+                ,
+              )
+            )
+            ],
+          ),
+        ),
       
     );
   }
